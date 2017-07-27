12c34fa3049882e7d96c511b6bcc6f705aba7c0d
Milan Berka <berkamilanml@gmail.com>
2017-07-27 16:12:57 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,115 @@
+import pip
+pip.main(['install', '--disable-pip-version-check', '--no-cache-dir', 'pydrive'])
+
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+from io import BytesIO
+import os 
+import zipfile 
+import pandas as pd
+import numpy as np
+import xml.etree.ElementTree as ET
+import glob
+from keboola import docker
+
+""" =========================== """
+"""     PARSER DEFINITON        """
+""" =========================== """
+
+# MAIN PARSER CLASSES
+def return_transactions_df(root):
+    """ Takes in root of a CSOB disbursement XML file and finds transactions,
+    then returns them as pandas DataFrame """
+    
+    transactions_list = [] # keeps the dictionaries, where one dictionary is one row
+    # Iterate through the XML file
+    for merchant in root.iter('merchant'):
+        merchant_header_dict = {}               # keeps the info about the merchant (in merchant header)
+        # Get information from the `merchant_header`
+        merchant_header = merchant.findall('merchant_header')
+        for merchant_header_cells in merchant_header[0].findall('./*'):
+            merchant_header_dict['merchant_'+merchant_header_cells.tag] = merchant_header_cells.text
+        
+        # Get information from the `transaction`s
+        for merchant_transaction in merchant.iter('transaction'):
+            merchant_transaction_dict = {}
+            for merchant_transaction_cell in merchant_transaction.findall('./*'): 
+                merchant_transaction_dict['transaction_'+merchant_transaction_cell.tag] = merchant_transaction_cell.text
+            
+            # append one row to the final dataframe, if the row is not empty (i.e. there is at least one transaction)
+            if not merchant_transaction_dict:
+                pass
+            else:
+                transactions_list.append({**merchant_header_dict,**merchant_transaction_dict})
+    return pd.DataFrame(transactions_list) 
+        
+""" =========================== """
+"""    GOOGLE API CONNECTION    """
+""" =========================== """
+
+if __name__ == '__main__': 
+    
+    """ =========================== """    
+    """ LOAD ALREADY PROCESSED DATA """
+    """ =========================== """   
+    try:
+        alreadyProcessedZipfiles = pd.read_csv('in/tables/alreadyProcessedZipFiles.csv')
+    except: 
+        alreadyProcessedZipfiles = pd.DataFrame({'name':[]})
+    
+    """ =========================== """
+    """ KEBOOLA STUFF """
+    """ =========================== """
+    
+    cfg = docker.Config()
+    parameters = cfg.get_parameters()
+    folderNames = parameters.get('folderNames')
+    gauth = GoogleAuth(settings_file='/data/in/files/253425786_settings.yaml')
+    drive = GoogleDrive(gauth)
+    
+    """ =========================== """
+    """      FILL THE DATAFRAME     """
+    """ =========================== """
+        
+    finalDataFrame = None
+        
+    if folderNames:
+        FOLDERS_TO_LOOKAT = list(folderNames)
+    else:
+        FOLDERS_TO_LOOKAT = ['CSOB AM 2016','CSOB AM 2017'] 
+       
+    for folderToLookAt in FOLDERS_TO_LOOKAT:
+        driveFilesList = drive.ListFile({'q':"mimeType='application/vnd.google-apps.folder' and title='{}' and trashed=false".format(folderToLookAt)}).GetList()                        
+        folderId = driveFilesList[0]['id']
+        zipfilesInFolder = drive.ListFile({'q':"'{}' in parents".format(folderId)}).GetList()
+        for zf in zipfilesInFolder:
+            if ('zip' in zf['title'].lower()) & (zf['title'] not in alreadyProcessedZipfiles['name'].tolist()) :
+                print('title: {}'.format(zf['title']))
+                alreadyProcessedZipfiles = alreadyProcessedZipfiles.append(pd.DataFrame([{'name':zf['title']}]))
+                toUnzip = drive.CreateFile({'id':zf['id']})
+                toUnzipStringContent = toUnzip.GetContentString(encoding='cp862')
+                toUnzipBytesContent = BytesIO(toUnzipStringContent.encode('cp862'))
+                readZipfile = zipfile.ZipFile(toUnzipBytesContent, "r")
+                for fileInZipfileName in readZipfile.namelist():
+                    if '.xml' in fileInZipfileName.lower():
+                        if ('-t' in fileInZipfileName.lower()) | ('-m' in fileInZipfileName.lower()):
+                            pass
+                        else:
+                            openedXml = readZipfile.open(fileInZipfileName).read()
+                            loadedXml = ET.fromstring(openedXml.decode())
+                            toBeParsed = return_transactions_df(loadedXml)  
+                            transactionDataFrame = toBeParsed.parseToDataFrame()
+                            if finalDataFrame is not None:
+                                finalDataFrame = pd.concat([finalDataFrame.copy(),transactionDataFrame.copy()])
+                            else:
+                                finalDataFrame = transactionDataFrame.copy()
+                    else:
+                        pass              
+            else:
+                pass
+            
+    """ =========================== """
+    """      CONCAT NEW WITH OLD    """
+    """ =========================== """
+    finalDataFrame.to_csv('out/tables/parsedBatch.csv',index=None)
+    alreadyProcessedZipfiles.to_csv('out/tables/alreadyProcessedZipfiles.csv',index=None)
